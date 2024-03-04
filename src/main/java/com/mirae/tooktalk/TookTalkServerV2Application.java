f7cb241723fac1fad087ef30ff09c31543a32141
최미래 <miraechoi@dgsw.hs.kr>
2024-03-04 09:09:24 +0900

    init

---
+++ src/main/java/com/mirae/tooktalk/TookTalkServerV2Application.java
@@ -0,0 +1,13 @@
+package com.mirae.tooktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TookTalkServerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TookTalkServerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mirae/tooktalk/TookTalkServerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirae.tooktalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TookTalkServerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

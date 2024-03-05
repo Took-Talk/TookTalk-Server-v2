993d67ae5e7dddc683f868d170a46bdbfcf99530
최미래 <miraechoi@dgsw.hs.kr>
2024-03-06 08:40:50 +0900

    Feat :: Create WebSocketConfig

---
+++ src/main/java/com/mirae/tooktalk/global/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.mirae.tooktalk.global.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import com.mirae.tooktalk.domain.chat.ChatHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+@Slf4j
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final ChatHandler chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
+    }
+}

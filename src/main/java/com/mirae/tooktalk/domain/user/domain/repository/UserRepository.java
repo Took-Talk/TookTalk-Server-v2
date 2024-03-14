a52252c167db3f8d255d6c7350e87cf450388197
최미래 <miraechoi@dgsw.hs.kr>
2024-03-14 09:41:02 +0900

    Add :: Add @Repository and findById

--- src/main/java/com/mirae/tooktalk/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/mirae/tooktalk/domain/user/domain/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.mirae.tooktalk.domain.user.domain.repository;
 
 import com.mirae.tooktalk.domain.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findById(Long userId);
 }

94fa1ce0e197a3ae3508fa546712bbd0cf4b296f
최미래 <miraechoi@dgsw.hs.kr>
2024-03-14 18:36:22 +0900

    Feat :: Create user&edit dto

---
+++ src/main/java/com/mirae/tooktalk/domain/user/dto/EditDTO.java
@@ -0,0 +1,12 @@
+package com.mirae.tooktalk.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EditDTO {
+
+    private String name;
+
+}
---
+++ src/main/java/com/mirae/tooktalk/domain/user/dto/UserDTO.java
@@ -0,0 +1,45 @@
+package com.mirae.tooktalk.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    /*
+        전화번호
+    */
+    private String number;
+
+    /*
+        닉네임
+    */
+    private String username;
+
+    /*
+        비밀번호
+    */
+    private String password;
+
+    /*
+        나이
+    */
+    private String age;
+
+    /*
+        성별
+    */
+    private String gender;
+    /*
+        관심사
+    */
+    private List<String> interests;
+
+    /*
+        자기 소개
+    */
+    private String bio;
+}

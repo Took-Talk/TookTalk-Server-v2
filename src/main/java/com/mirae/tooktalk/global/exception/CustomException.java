749f0b751556ead3a290253ec3a3369d566b316c
최미래 <miraechoi@dgsw.hs.kr>
2024-03-15 11:44:47 +0900

    Remove :: 예외처리 관련 파일 삭제

--- src/main/java/com/mirae/tooktalk/global/exception/CustomException.java
+++
@@ -1,8 +0,0 @@
-package com.mirae.tooktalk.global.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class CustomException {
-    public CustomException(HttpStatus badRequest, String s) {
-    }
-}

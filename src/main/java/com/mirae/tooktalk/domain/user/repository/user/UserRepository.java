4ce2f622dad2fc35c3f70b33a5d47fba835b49e9
최미래 <miraechoi@dgsw.hs.kr>
2024-04-04 11:52:12 +0900

    Remove :: findByNumber 제거

--- src/main/java/com/mirae/tooktalk/domain/user/repository/user/UserRepository.java
+++ src/main/java/com/mirae/tooktalk/domain/user/repository/user/UserRepository.java
@@ -1,7 +1,6 @@
 package com.mirae.tooktalk.domain.user.repository.user;
 
 import com.mirae.tooktalk.domain.user.entity.user.UserEntity;
-import com.mirae.tooktalk.domain.user.payload.request.UserInfoRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByNumber(String number);
-    Optional<UserInfoRequest> findByNickname(String nickname);
     Boolean existsByNumber(String number);
 
     Boolean existsByNickname(String nickname);

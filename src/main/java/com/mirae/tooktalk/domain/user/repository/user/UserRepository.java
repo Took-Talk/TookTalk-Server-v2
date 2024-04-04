7de75ca19600c6914c1a3902343ee183ffe30c3d
최미래 <miraechoi@dgsw.hs.kr>
2024-04-04 11:21:03 +0900

    Feat :: create findbynickname

--- src/main/java/com/mirae/tooktalk/domain/user/repository/user/UserRepository.java
+++ src/main/java/com/mirae/tooktalk/domain/user/repository/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.mirae.tooktalk.domain.user.repository.user;
 
 import com.mirae.tooktalk.domain.user.entity.user.UserEntity;
+import com.mirae.tooktalk.domain.user.payload.request.UserInfoRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByNumber(String number);
+    Optional<UserInfoRequest> findByNickname(String nickname);
     Boolean existsByNumber(String number);
 
     Boolean existsByNickname(String nickname);

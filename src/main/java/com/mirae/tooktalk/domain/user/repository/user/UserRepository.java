9db0e25df37f181286a060da3723f552fe1565e5
최미래 <miraechoi@dgsw.hs.kr>
2024-04-10 22:52:14 +0900

    Feat :: create findByMbtiAndAgeAndGender

--- src/main/java/com/mirae/tooktalk/domain/user/repository/user/UserRepository.java
+++ src/main/java/com/mirae/tooktalk/domain/user/repository/user/UserRepository.java
@@ -4,6 +4,7 @@ import com.mirae.tooktalk.domain.user.entity.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,6 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Boolean existsByNickname(String nickname);
 
     Optional<UserEntity> findByNicknameEquals(String nickname);
+
+    List<UserEntity> findByMbtiAndAgeAndGender(String mbti, String age, String gender);
 }

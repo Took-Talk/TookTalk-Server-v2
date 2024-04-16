018d360e07b8614978c885d07b5a938959a113a3
최미래 <miraechoi@dgsw.hs.kr>
2024-04-16 19:56:30 +0900

    Improve :: improve enterchatroom

--- src/main/java/com/mirae/tooktalk/domain/chat/controller/ChatController.java
+++ src/main/java/com/mirae/tooktalk/domain/chat/controller/ChatController.java
@@ -1,7 +1,6 @@
 package com.mirae.tooktalk.domain.chat.controller;
 
 import com.mirae.tooktalk.domain.chat.dto.ChatMessageDto;
-import com.mirae.tooktalk.domain.chat.service.ChatService;
 import com.mirae.tooktalk.domain.user.payload.request.MatchingRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -17,21 +16,12 @@ import org.springframework.stereotype.Controller;
 @Tag(name = "채팅", description = "채팅 관련 API")
 public class ChatController {
 
-    private ChatService chatService;
-
     private final SimpMessagingTemplate template;
 
     @Operation(summary = "채팅방 입장", description = "채팅 방에 입장합니다.")
     @MessageMapping("/chat/enter")
-    public void enterChatRoom(@Payload MatchingRequest  matchingRequest, Authentication authentication) {
-        String roomId = chatService.matchAndEnterChatRoom(matchingRequest, authentication);
-        if (roomId != null) {
-            /* 매칭된 채팅방의 roomId를 프론트엔드에게 전송 */
-            template.convertAndSend("/sub/chat/room/" + roomId);
-        } else {
-            /* 매칭된 유저가 없을 경우에 대한 처리 */
-            template.convertAndSend("/sub/chat/room/error", "매칭할 유저가 없습니다.");
-        }
+    public void enterChatRoom(ChatMessageDto message) {
+        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 
     @Operation(summary = "채팅 메세지", description = "메세지를 전송합니다.")

a08dd27f5fa138cb736dcec520394ffc9e0078aa
최미래 <miraechoi@dgsw.hs.kr>
2024-04-16 20:28:24 +0900

    Add :: add swagger description

--- src/main/java/com/mirae/tooktalk/domain/chat/controller/MatchingController.java
+++ src/main/java/com/mirae/tooktalk/domain/chat/controller/MatchingController.java
@@ -1,6 +1,8 @@
 package com.mirae.tooktalk.domain.chat.controller;
 
 import com.mirae.tooktalk.domain.chat.service.MatchingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,11 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "매칭", description = "매 관련 API")
 public class MatchingController {
     private final MatchingService matchingService;
 
+    @Operation(summary = "mbti 매칭", description = "mbti로 유저를 매칭합니다.")
     @PostMapping("/matching/{mbti}")
     public Map<String, Integer> matching(
             Authentication authentication,

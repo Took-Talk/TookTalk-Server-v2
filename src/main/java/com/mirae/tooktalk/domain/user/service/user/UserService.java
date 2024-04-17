17052c3f0a86d0e33c94d18630d45155746285a4
최미래 <miraechoi@dgsw.hs.kr>
2024-04-17 20:28:17 +0900

    Feat :: change profile pic to userfix

--- src/main/java/com/mirae/tooktalk/domain/user/service/user/UserService.java
+++ src/main/java/com/mirae/tooktalk/domain/user/service/user/UserService.java
@@ -3,8 +3,11 @@ package com.mirae.tooktalk.domain.user.service.user;
 
 import com.mirae.tooktalk.domain.user.exception.CustomException;
 import com.mirae.tooktalk.domain.user.payload.request.SignupRequest;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface UserService {
 
-    void registerUser(SignupRequest signupRequest) throws CustomException;
+    void registerUser(SignupRequest signupRequest, MultipartFile multipartFile) throws CustomException, IOException;
 }

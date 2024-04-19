a72ade5656402ca0db33aa1190bb3282dabd7310
최미래 <miraechoi@dgsw.hs.kr>
2024-04-19 22:47:21 +0900

    Refactor :: matchingservice 반환값 수정

--- src/main/java/com/mirae/tooktalk/domain/chat/service/MatchingService.java
+++ src/main/java/com/mirae/tooktalk/domain/chat/service/MatchingService.java
@@ -2,6 +2,7 @@ package com.mirae.tooktalk.domain.chat.service;
 
 import com.mirae.tooktalk.domain.chat.entity.MatchingUserEntity;
 import com.mirae.tooktalk.domain.chat.repository.MatchingRepository;
+import com.mirae.tooktalk.domain.user.entity.user.UserEntity;
 import com.mirae.tooktalk.domain.user.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -17,23 +18,28 @@ public class MatchingService {
     private final UserRepository userRepository;
     private final MatchingRepository matchingRepository;
 
-    public Map<String, Integer> matching(Authentication authentication, String mbti){
-        int userId = userRepository.findByNicknameEquals(authentication.getName()).get().getId();
-        Map<String, Integer> map = new HashMap();
+    public Map<String, Object> matching(Authentication authentication, String mbti){
+        UserEntity user = userRepository.findByNicknameEquals(authentication.getName()).get();
+        user.hidePassword("");
+        Map<String, Object> map = new HashMap();
         MatchingUserEntity matchingUserEntity = matchingRepository.findByMbti(mbti);
         if (matchingUserEntity == null){
             int roomId = matchingRepository.save(
                     MatchingUserEntity.builder()
-                            .userId(userId)
+                            .userId(user.getId())
                             .mbti(mbti)
                             .build()
             ).getRoomId();
 
             map.put("roomId" , roomId);
+            map.put("userInfo",user);
+            map.put("matching", false);
             return map;
         } else{
             matchingRepository.delete(matchingUserEntity);
             map.put("roomId" , matchingUserEntity.getRoomId());
+            map.put("userInfo", user);
+            map.put("matching", true);
             return map;
         }
 

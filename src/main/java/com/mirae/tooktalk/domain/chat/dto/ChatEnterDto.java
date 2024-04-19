19a6c5fe8238513c45f64437224225bd0a47018d
최미래 <miraechoi@dgsw.hs.kr>
2024-04-19 22:48:56 +0900

    Feat :: create chatenterdto

---
+++ src/main/java/com/mirae/tooktalk/domain/chat/dto/ChatEnterDto.java
@@ -0,0 +1,19 @@
+package com.mirae.tooktalk.domain.chat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+
+public class ChatEnterDto {
+    private String roomId;
+    private String nickname;
+    private String mbti;
+    private String bio;
+    private List<String> interests;
+    private String message;
+    private String imgUrl;
+}

a877651b6215db8e66320e99686f04ce2048137a
최미래 <miraechoi@dgsw.hs.kr>
2024-04-22 17:13:43 +0900

    Refactor :: 단일 책임 원칙 적용 #35

--- src/main/java/com/mirae/tooktalk/domain/chat/service/MatchingService.java
+++ src/main/java/com/mirae/tooktalk/domain/chat/service/MatchingService.java
@@ -21,27 +21,40 @@ public class MatchingService {
     public Map<String, Object> matching(Authentication authentication, String mbti){
         UserEntity user = userRepository.findByNicknameEquals(authentication.getName()).get();
         user.hidePassword("");
-        Map<String, Object> map = new HashMap();
+        Map<String, Object> map = new HashMap<>();
         MatchingUserEntity matchingUserEntity = matchingRepository.findByMbti(mbti);
         if (matchingUserEntity == null){
-            int roomId = matchingRepository.save(
-                    MatchingUserEntity.builder()
-                            .userId(user.getId())
-                            .mbti(mbti)
-                            .build()
-            ).getRoomId();
-
-            map.put("roomId" , roomId);
-            map.put("userInfo",user);
-            map.put("matching", false);
-            return map;
+            map = addWaitingList(user, mbti);
         } else{
-            matchingRepository.delete(matchingUserEntity);
-            map.put("roomId" , matchingUserEntity.getRoomId());
-            map.put("userInfo", user);
-            map.put("matching", true);
-            return map;
+            map = removeFromWaitingList(user, matchingUserEntity);
         }
+        return map;
+    }
 
+    /* 해당하는 유저가 없을 경우 대기열에 추가하는 메서드 */
+    private Map<String, Object> addWaitingList(UserEntity user, String mbti) {
+        int roomId = matchingRepository.save(
+                MatchingUserEntity.builder()
+                        .userId(user.getId())
+                        .mbti(mbti)
+                        .build()
+        ).getRoomId();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("roomId", roomId);
+        map.put("userInfo", user);
+        map.put("matching", false);
+        return map;
+    }
+
+    /* 매칭 시 본인 데이터를 대기열에서 지우는 메서드 */
+    private Map<String, Object> removeFromWaitingList(UserEntity user, MatchingUserEntity matchingUserEntity) {
+        matchingRepository.delete(matchingUserEntity);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("roomId", matchingUserEntity.getRoomId());
+        map.put("userInfo", user);
+        map.put("matching", true);
+        return map;
     }
 }

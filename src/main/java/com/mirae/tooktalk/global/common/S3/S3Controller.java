117d0327680c2302f171aa740268e5e4db85190b
최미래 <miraechoi@dgsw.hs.kr>
2024-04-29 23:04:26 +0900

    Refactor :: refactor authenticateUser

--- src/main/java/com/mirae/tooktalk/global/common/S3/S3Controller.java
+++ src/main/java/com/mirae/tooktalk/global/common/S3/S3Controller.java
@@ -1,10 +1,14 @@
 package com.mirae.tooktalk.global.common.S3;
 
+import com.mirae.tooktalk.global.common.S3.dto.S3Dto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -19,7 +23,7 @@ public class S3Controller {
 
     @Operation(summary = "업로드", description = "파일을 업로드 합니다.")
     @PostMapping("/upload")
-    public ResponseEntity<?> authenticateUser(@RequestPart("image") MultipartFile multipartFile) throws IOException {
-        return ResponseEntity.ok().body(s3Uploader.upload(multipartFile, "profile pic"));
+    public ResponseEntity<S3Dto> authenticateUser(@RequestPart("image") MultipartFile multipartFile) throws IOException {
+        return ResponseEntity.ok().body(S3Dto.builder().imgUrl(s3Uploader.upload(multipartFile, "picture")).build());
     }
 }

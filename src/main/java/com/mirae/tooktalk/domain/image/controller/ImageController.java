b002c6db2034f824f0bf421b1715d33714625b06
최미래 <miraechoi@dgsw.hs.kr>
2024-04-30 13:22:06 +0900

    Feat :: s3 controller -> image controller
    + 비지니스 로직 이동으로 인한 코드 수정

--- src/main/java/com/mirae/tooktalk/domain/image/controller/ImageController.java
+++ src/main/java/com/mirae/tooktalk/domain/image/controller/ImageController.java
@@ -1,5 +1,6 @@
-package com.mirae.tooktalk.global.common.S3;
+package com.mirae.tooktalk.domain.image.controller;
 
+import com.mirae.tooktalk.domain.image.service.ImageService;
 import com.mirae.tooktalk.global.common.S3.dto.S3Dto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,19 +12,17 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @Tag(name = "파일", description = "파일 업로드 api 입니다.")
 @RestController
 @RequestMapping("/file")
 @RequiredArgsConstructor
-public class S3Controller {
+public class ImageController {
 
-    private final S3Uploader s3Uploader;
+    private final ImageService imageService;
 
     @Operation(summary = "업로드", description = "파일을 업로드 합니다.")
     @PostMapping("/upload")
-    public ResponseEntity<S3Dto> authenticateUser(@RequestPart("image") MultipartFile multipartFile) throws IOException {
-        return ResponseEntity.ok().body(S3Dto.builder().imgUrl(s3Uploader.upload(multipartFile, "picture")).build());
+    public ResponseEntity<S3Dto> uploadImage(@RequestPart("image") MultipartFile multipartFile) {
+        return ResponseEntity.ok().body(imageService.uploadImage(multipartFile));
     }
 }
